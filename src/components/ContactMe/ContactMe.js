68f6dfb08cc25df4a297c54a0a6715fbe22a6604
Karima-Aktari <karimaaktari911@gmail.com>
2022-04-17 15:41:11 +0600

    project add

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -16,19 +16,19 @@ const ContactMe = () => {
 
     return (
         <div id="contact" className="container border contact-form">
-            <div className="my-4 py-4">
-                <h1>This is contact</h1>
-                <form className="row" style={{ margin: "25px 85px 75px 100px" }}
+            <div className="my-4 py-4 px-4">
+                <h1>Contact me </h1>
+                <form className="row p-5" style={{ margin: "25px 85px 75px 100px" }}
                     onSubmit={sendEmail}>
                     <label>Name</label>
-                    <input type="text" name="name" className="form-control" />
+                    <input type="text" name="name" className="form-control my-2" />
 
                     <label>Email</label>
-                    <input type="email" name="user-email" className="form-control" />
+                    <input type="email" name="user-email" className="form-control my-2" />
 
                     <label>Message</label>
-                    <textarea name="message" rows='5' />
-                    <input type="submit" value="submit" className="form-control btn btn-warning mt-4 py-2" />
+                    <textarea name="message" className='my-2' rows='5' />
+                    <input type="submit" value="submit" className="form-control btn btn-warning mt-4 py-2 my-2" />
                 </form>
             </div>
         </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
     }, [])
     console.log(projects);
     return (
-        <div id="projects" className="mx-auto bg-projects py-3">
+        <div id="projects" className="mx-auto bg-projects py-5">
             <h1>My Projects</h1>
             <div className="container row mx-auto py-3">
                 {
@@ -28,3 +28,23 @@ const Projects = () => {
 };
 
 export default Projects;
+
+
+
+// {
+//     "id": "5",
+//     "img1": "https://i.ibb.co/DwBrDLv/Project4-1.jpg",
+//     "img2": "https://i.ibb.co/54Tzhnx/Project4-2.jpg",
+//     "img3": "https://i.ibb.co/K2XSkkJ/Project4-3.jpg",
+//     "img4": "https://i.ibb.co/syJTr74/Project4-4.jpg",
+//     "name": "Honda CBR 300R",
+//     "description1": "It's a Fully responsive site.",
+//     "description2": "For css framework i have used bootstrap",
+//     "description3": "This site shows only UI",
+//     "description4": "Here to show product collection i have used bootstrap slider",
+//     "description5": "To control responsiveness i apply media@query",
+//     "technology": "HTML,CSS,Bootstrap,React js,Font Awesome,Netlify",
+//     "live": "https://unruffled-goldwasser-cbr-honda-bootstrap.netlify.app/",
+//     "client": "https://github.com/Karima-Aktari/honda-cbr-bootstrap-assignment-3rd",
+//     "server": "No Need"
+// }
